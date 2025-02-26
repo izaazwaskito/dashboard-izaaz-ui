7c760f3c10dc14398224578225c2f19fadc6339a
Izaaz Waskito <adidata.izaaz@bankmandiri.co.id>
2025-02-26 10:14:25 +0700

    fixing

--- app/page.tsx
+++ app/page.tsx
@@ -52,9 +52,8 @@ import {
 import { MainNav } from "@/components/navigation/MainNav";
 
 const Home = () => {
-  const { theme, setTheme } = useTheme();
+  const { theme} = useTheme();
   const [mounted, setMounted] = React.useState(false);
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const fadeInUp = {
     hidden: { opacity: 0, y: 50 },
     visible: {
